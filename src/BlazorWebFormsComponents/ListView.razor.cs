276ab12a3ee7b24d6c45ede570d538f53f62cb06
Jeff Fritz <csharpfritz@users.noreply.github.com>
2019-12-04 23:15:44 -0700

    Created ListView partial class

---
+++ src/BlazorWebFormsComponents/ListView.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWebFormsComponents
+{
+
+  public partial class ListView<TItem>
+  {
+
+    [Parameter]
+    public IEnumerable<TItem> Items { get; set; }
+
+    [Parameter]
+    public RenderFragment EmptyDataTemplate { get; set; }
+
+    [Parameter]
+    public RenderFragment TableHeader { get; set; }
+
+    [Parameter]
+    public RenderFragment<TItem> RowTemplate { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object> AdditionalAttributes { get; set; }
+  }
+
+}
