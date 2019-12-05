11e2b0de0f567e46df501104759e09caad10a27d
Jeff Fritz <csharpfritz@users.noreply.github.com>
2019-12-05 09:47:48 -0700

    Finished a sample page for the ListView in web forms

---
+++ samples/BeforeWebForms/Models/Widget.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeforeWebForms.Models
+{
+  public class Widget
+  {
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime LastUpdate { get; set; }
+
+  }
+}
