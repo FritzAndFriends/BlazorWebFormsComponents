02b834caf992829e7e619278540bff894f7aabbb
Kahbazi <A.Kahbazi@gmail.com>
2020-01-09 17:01:53 +0330

    Add style to BaseValidator

--- src/BlazorWebFormsComponents/Validations/BaseValidator.razor.cs
+++ src/BlazorWebFormsComponents/Validations/BaseValidator.razor.cs
@@ -1,20 +1,42 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq.Expressions;
 using System.Reflection;
+using BlazorWebFormsComponents.Enums;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace BlazorWebFormsComponents.Validations
 {
-	public abstract partial class BaseValidator<Type> : BaseWebFormsComponent
+	public abstract partial class BaseValidator<Type> : BaseWebFormsComponent, IHasStyle
 	{
 		[CascadingParameter] EditContext CurrentEditContext { get; set; }
 
 		[Parameter] public ForwardRef<InputBase<Type>> ControlToValidate { get; set; }
 		[Parameter] public string Text { get; set; }
 		[Parameter] public string ErrorMessage { get; set; }
+		[Parameter] public Color ForeColor { get; set; }
 
 		public bool IsValid { get; set; } = true;
+
+		[Parameter] public Color BackColor { get; set; }
+		[Parameter] public Color BorderColor { get; set; }
+		[Parameter] public BorderStyle BorderStyle { get; set; }
+		[Parameter] public Unit BorderWidth { get; set; }
+		[Parameter] public string CssClass { get; set; }
+		[Parameter] public Unit Height { get; set; }
+		[Parameter] public HorizontalAlign HorizontalAlign { get; set; }
+		[Parameter] public VerticalAlign VerticalAlign { get; set; }
+		[Parameter] public Unit Width { get; set; }
+		[Parameter] public bool Font_Bold { get; set; }
+		[Parameter] public bool Font_Italic { get; set; }
+		[Parameter] public string Font_Names { get; set; }
+		[Parameter] public bool Font_Overline { get; set; }
+		[Parameter] public FontUnit Font_Size { get; set; }
+		[Parameter] public bool Font_Strikeout { get; set; }
+		[Parameter] public bool Font_Underline { get; set; }
+
+		protected string CalculatedStyle { get; set; }
 
 		protected override void OnInitialized()
 		{
@@ -22,6 +44,10 @@ namespace BlazorWebFormsComponents.Validations
 			var messages = new ValidationMessageStore(CurrentEditContext);
 
 			CurrentEditContext.OnValidationRequested += (sender, eventArgs) => EventHandler((EditContext)sender, messages);
+
+			this.SetFontsFromAttributes(AdditionalAttributes);
+
+			CalculatedStyle = this.ToStyleString();
 
 			base.OnInitialized();
 		}
