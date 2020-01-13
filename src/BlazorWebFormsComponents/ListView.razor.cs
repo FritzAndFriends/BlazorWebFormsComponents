814b6a12a5e93549a74e14d6f5c14ad7b180608e
SQL-MisterMagoo <mister.magoo+githubsql@gmail.com>
2020-01-13 16:39:50 +0000

    Implement LayoutTemplate (#32)
    Fixed params on OnItemDataBound as they were EventArgs, so couldn't pass an item.
    Modified LayoutTest sample slightly to make sure alternating item template works
    and to make sure the result is obviously different to the simple sample.

--- src/BlazorWebFormsComponents/ListView.razor.cs
+++ src/BlazorWebFormsComponents/ListView.razor.cs
@@ -7,37 +7,37 @@ using System.Threading.Tasks;
 
 namespace BlazorWebFormsComponents
 {
- 
-	public partial class ListView<ItemType> : BaseModelBindingComponent<ItemType>
-  {
 
-    public ListView()
-    {
-    }
+	public partial class ListView<ItemType> : BaseModelBindingComponent<ItemType>
+	{
+
+		public ListView()
+		{
+		}
 
 		#region Templates
 
 		[Parameter]
-    public RenderFragment<ItemType> AlternatingItemTemplate { get; set; }
+		public RenderFragment<ItemType> AlternatingItemTemplate { get; set; }
 
-    /// <summary>
-    /// Defines the content to render if the data source returns no data.
-    /// </summary>
-    [Parameter]
-    public RenderFragment EmptyDataTemplate { get; set; }
+		/// <summary>
+		/// Defines the content to render if the data source returns no data.
+		/// </summary>
+		[Parameter]
+		public RenderFragment EmptyDataTemplate { get; set; }
 
-    [Parameter]
-    public RenderFragment ItemSeparatorTemplate { get; set; }
+		[Parameter]
+		public RenderFragment ItemSeparatorTemplate { get; set; }
 
-    [Parameter]
-    public RenderFragment<ItemType> ItemTemplate { get; set; }
+		[Parameter]
+		public RenderFragment<ItemType> ItemTemplate { get; set; }
 
-    /// <summary>
-    /// 🚨🚨 LayoutTemplate is not available.  Please wrap the ListView component with the desired layout 🚨🚨
-    /// </summary>
-    [Parameter]
-    [Obsolete("The LayoutTemplate child element is not supported in Blazor.  Instead, wrap the ListView component with the desired layout")]
-    public RenderFragment LayoutTemplate { get; set; }
+		/// <summary>
+		/// 🚨🚨 LayoutTemplate is not available.  Please wrap the ListView component with the desired layout 🚨🚨
+		/// </summary>
+		[Parameter]
+		//[Obsolete("The LayoutTemplate child element is not supported in Blazor.  Instead, wrap the ListView component with the desired layout")]
+		public RenderFragment<RenderFragment> LayoutTemplate { get; set; }
 
 		/// <summary>
 		/// 🚨🚨 LayoutTemplate and the OnLayoutCreated event is not available.  Please wrap the ListView component with the desired layout 🚨🚨
@@ -49,16 +49,16 @@ namespace BlazorWebFormsComponents
 		#endregion
 
 		[Parameter] // TODO: Implement
-    public InsertItemPosition InsertItemPosition { get; set; }
+		public InsertItemPosition InsertItemPosition { get; set; }
 
-    [Parameter] // TODO: Implement
-    public int SelectedIndex { get; set; }
+		[Parameter] // TODO: Implement
+		public int SelectedIndex { get; set; }
 
-    /// <summary>
-    /// Style is not applied by this control
-    /// </summary>
-    [Parameter, Obsolete("Style is not applied by this control")]
-    public string Style { get; set; }
+		/// <summary>
+		/// Style is not applied by this control
+		/// </summary>
+		[Parameter, Obsolete("Style is not applied by this control")]
+		public string Style { get; set; }
 
 
 		[Parameter]
