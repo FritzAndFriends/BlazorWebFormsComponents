45940317563303098d2b24d085cd132d3bfcf68c
Kahbazi <A.Kahbazi@gmail.com>
2020-01-17 14:25:39 +0330

    Added LoginName

--- samples/AfterBlazorClientSide/Startup.cs
+++ samples/AfterBlazorClientSide/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace AfterBlazorClientSide
 	{
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 		}
 
 		public void Configure(IComponentsApplicationBuilder app)
---
+++ samples/AfterBlazorServerSide/CustomAuthStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace AfterBlazorServerSide
+{
+	public class CustomAuthStateProvider : AuthenticationStateProvider
+	{
+		public override Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			var identity = new ClaimsIdentity(new[]
+			{
+				new Claim(ClaimTypes.Name, "James Bond"),
+			}, "Fake authentication type");
+
+			var user = new ClaimsPrincipal(identity);
+
+			return Task.FromResult(new AuthenticationState(user));
+		}
+	}
+}
