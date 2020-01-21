f2c132d83e30c326735fdb32ef6b6077457c504b
Kahbazi <A.Kahbazi@gmail.com>
2020-01-21 17:52:12 +0330

    Add LoginName and LoginStatus tests

---
+++ src/BlazorWebFormsComponents.Test/MockNavigationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebFormsComponents.Test
+{
+  public class MockNavigationManager : NavigationManager
+  {
+
+		public MockNavigationManager()
+		{
+
+			Initialize("http://localhost/", "http://localhost/");
+
+		}
+
+		public string LastUri { get; set; }
+
+		protected override void NavigateToCore(string uri, bool forceLoad)
+		{
+
+		  LastUri = uri;
+
+		}
+
+  }
+
+}
