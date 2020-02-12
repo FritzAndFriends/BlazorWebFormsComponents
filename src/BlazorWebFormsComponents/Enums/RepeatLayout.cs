aa5b8356220243d5ca606ec134a8c4a5de97ac7d
Gareth Leachman <grleachman@users.noreply.github.com>
2020-02-12 15:36:47 +0000

    Data list repeater column(#21) (#78)
    * DataList bind item and data  (#63)
    
    * (#21) implement Vertical algorithm

--- src/BlazorWebFormsComponents/Enums/RepeatLayout.cs
+++ src/BlazorWebFormsComponents/Enums/RepeatLayout.cs
@@ -6,13 +6,9 @@ namespace BlazorWebFormsComponents.Enums
 {
 	public abstract class RepeatLayout
 	{
-
 		public static TableRepeatLayout Table { get; } = new TableRepeatLayout();
 		public static FlowRepeatLayout Flow { get; } = new FlowRepeatLayout();
-
 	}
-
 	public class TableRepeatLayout : RepeatLayout { }
 	public class FlowRepeatLayout : RepeatLayout { }
-
 }
