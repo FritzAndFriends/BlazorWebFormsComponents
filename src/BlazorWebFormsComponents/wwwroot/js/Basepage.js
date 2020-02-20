770275b7e1f5ef97e3203089acfbaf6ab9580336
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-02-20 12:58:22 -0500

    Started a little JS interop to make Title work

---
+++ src/BlazorWebFormsComponents/wwwroot/js/Basepage.js
@@ -0,0 +1,16 @@
+(function(){
+
+  var Page = {
+
+    setTitle = function(title) {
+      
+      window.title = title;
+
+    }
+
+  }
+
+  window.bwfc = window.bwfc ?? {};
+  window.bwfc.Page = Page;
+
+})();
