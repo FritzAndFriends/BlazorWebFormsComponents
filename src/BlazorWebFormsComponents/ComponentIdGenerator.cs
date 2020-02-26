fe17892fe4c5aad568fb603215a96fd144938b88
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-02-26 17:14:47 +0300

    Add docs

--- src/BlazorWebFormsComponents/ComponentIdGenerator.cs
+++ src/BlazorWebFormsComponents/ComponentIdGenerator.cs
@@ -2,10 +2,21 @@
 
 namespace BlazorWebFormsComponents
 {
+	/// <summary>
+	/// Represents a utility class to generate the component Id.
+	/// </summary>
   public class ComponentIdGenerator
   {
+		/// <summary>
+		/// Generates an Id for a given component.
+		/// </summary>
+		/// <param name="component">The <see cref="BaseWebFormsComponent"/>.</param>
+		/// <returns>The generated component Id.</returns>
+		/// <remarks>The generated Id format is {ComponentType}_{GUID} if the <see cref="BaseWebFormsComponent.ID"/> is not set.</remarks>
+		/// <example>Login_8a1112a8-bdf3-4a3c-91f6-caa9ee1ff974.</example>
 		public static string Generate(BaseWebFormsComponent component)
 		{
+			// TODO: Use ClientIDMode
 			var generatedId = component.ID;
 			if (!string.IsNullOrEmpty(generatedId))
 			{
