679cec305ed4c2acb892c9e636aaa54e77b5e555
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-03-05 12:07:49 -0500

    Only outputting to the console where ASPNETCORE_Environment is Production

--- src/BlazorWebFormsComponents.Test/TestConsole.cs
+++ src/BlazorWebFormsComponents.Test/TestConsole.cs
@@ -3,12 +3,14 @@ using System;
 namespace BlazorWebFormsComponents.Test {
 
   public static class TestConsole {
+		
+    private static string envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    private static bool hasTargetEnvironment = (string.IsNullOrEmpty(envName) || envName != "Production");
 
     public static void WriteLine(string text) {
 
-      string envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-      if (string.IsNullOrEmpty(envName) || envName != "Production")
-        Console.WriteLine(text);
+			System.Diagnostics.Debug.WriteLineIf(hasTargetEnvironment, text);
+			return;
 
     }
 
