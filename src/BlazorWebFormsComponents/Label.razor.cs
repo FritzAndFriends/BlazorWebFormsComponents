7a56c5f1aa723ec00c5b90f545a1d816a75fc090
JuViquez <33298903+JuViquez@users.noreply.github.com>
2020-03-09 18:43:00 -0600

    Template Fields gridview

---
+++ src/BlazorWebFormsComponents/Label.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebFormsComponents
+{
+  public partial class Label : BaseWebFormsComponent
+  {
+		[Parameter] public object Text { get; set; }
+	}
+}
