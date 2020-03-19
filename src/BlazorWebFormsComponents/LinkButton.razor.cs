2dbb321b7a24a99026d37069ceeeb19f4a8f68c6
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-03-19 18:10:30 +0300

    Add LinkButton control

---
+++ src/BlazorWebFormsComponents/LinkButton.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorWebFormsComponents
+{
+	public partial class LinkButton : BaseWebFormsComponent
+	{
+		[Parameter]
+		public string PostBackUrl { get; set; }
+
+		[Parameter]
+		public string Text { get; set; }
+
+		[Parameter]
+		public Action OnClick { get; set; }
+
+		private async Task TriggerClick()
+		{
+			OnClick?.Invoke();
+		}
+	}
+}
