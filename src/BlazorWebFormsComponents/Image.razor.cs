1cec8afc4d78615fd1bbfa196b9b86067e7ec522
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-03-22 18:49:46 +0300

    Add Image control

---
+++ src/BlazorWebFormsComponents/Image.razor.cs
@@ -0,0 +1,17 @@
+using BlazorWebFormsComponents.Enums;
+using Microsoft.AspNetCore.Components;
+namespace BlazorWebFormsComponents {	public partial class Image : ButtonBaseComponent
+	{
+		[Parameter]
+		public string AlternateText { get; set; }
+
+		[Parameter]
+		public string DescriptionUrl { get; set; } = string.Empty;
+
+		[Parameter]
+		public ImageAlign ImageAlign { get; set; }
+
+		[Parameter]
+		public string ImageUrl { get; set; }
+	}
+}
