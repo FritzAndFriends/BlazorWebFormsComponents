a736ea95c75d099618d83efc315e8f76e3513c35
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-03-25 18:21:51 +0300

    Fix label HTML render output (#143)
    * Fix label HTML render output
    
    * Remove unnecessary ToString() from Text property

--- src/BlazorWebFormsComponents/Label.razor.cs
+++ src/BlazorWebFormsComponents/Label.razor.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorWebFormsComponents.Interfaces;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorWebFormsComponents
 {
-  public partial class Label : BaseWebFormsComponent
+  public partial class Label : BaseWebFormsComponent, ITextComponent
 	{ 
-		[Parameter] public object Text { get; set; }
-
+		[Parameter]
+		public string Text { get; set; }
 	}
 }
