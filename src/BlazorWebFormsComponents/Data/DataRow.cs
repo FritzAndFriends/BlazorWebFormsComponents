cc69ee4bd540f37d31d14969108136a284cf268d
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-08-02 12:11:30 -0400

    Started work on delegating to a BETTER DataTable implementation

---
+++ src/BlazorWebFormsComponents/Data/DataRow.cs
@@ -0,0 +1,14 @@
+﻿using System.Dynamic;
+
+namespace BlazorWebFormsComponents.Data
+{
+	public class DataRow : DynamicObject {
+
+		public override bool TryGetMember(GetMemberBinder binder, out object result)
+		{
+			return base.TryGetMember(binder, out result);
+		}
+
+	}
+
+}
---
+++ src/BlazorWebFormsComponents/Data/DataTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using DATA = System.Data;
+
+namespace BlazorWebFormsComponents.Data
+{
+
+	public class DataTable : IEnumerable<DataRow>
+	{
+		public IEnumerator<DataRow> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
