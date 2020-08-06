0e783b710573a5fbbbae95d72c5c52b27e72fcba
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-08-06 11:00:56 -0400

    Added WebForms configuration for publishing

---
+++ src/BlazorWebFormsComponents/Data/DataTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using DATA = System.Data;
+
+namespace BlazorWebFormsComponents.Data
+{
+
+	public class DataTable : IEnumerable<DataRow>
+	{
+
+		private DATA.DataTable _WrappedTable;
+
+		public IEnumerator<DataRow> GetEnumerator()
+		{
+			yield return _WrappedTable.Rows.GetEnumerator() as DATA.DataRow;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			yield return _WrappedTable.Rows.GetEnumerator();
+		}
+
+		public static implicit operator DataTable(DATA.DataTable table) {
+
+			return new DataTable { _WrappedTable = table };
+
+		}
+
+	}
+
+}
