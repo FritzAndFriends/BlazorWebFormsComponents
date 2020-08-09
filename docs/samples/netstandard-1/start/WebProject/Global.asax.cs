1dfa082a582f07b7306fdc9565e07c3208cf4d3c
Jeff Fritz <csharpfritz@users.noreply.github.com>
2020-08-09 08:54:47 -0400

    Moved and organized sample 1

--- docs/samples/netstandard-1/start/WebProject/Global.asax.cs
+++ docs/samples/netstandard-1/start/WebProject/Global.asax.cs
@@ -14,7 +14,6 @@ namespace WebProject
 		void Application_Start(object sender, EventArgs e)
 		{
 			// Code that runs on application startup
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
