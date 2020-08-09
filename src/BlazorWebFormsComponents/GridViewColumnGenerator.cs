fc1f556df064e728d07f602fd2fe3d0053472674
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-08-09 18:18:11 +0300

    GridView should support DataTable (#187)
    * GridView supports DataTable

--- src/BlazorWebFormsComponents/GridViewColumnGenerator.cs
+++ src/BlazorWebFormsComponents/GridViewColumnGenerator.cs
@@ -8,6 +8,8 @@ namespace BlazorWebFormsComponents
 	/// </summary>
   public static class GridViewColumnGenerator
   {
+		private const string IndexerPropertyName = "Item";
+
 		/// <summary>
 		/// Generate columns for a given GridView based on the properties of given Type
 		/// </summary>
@@ -16,8 +18,13 @@ namespace BlazorWebFormsComponents
 		public static void GenerateColumns<ItemType>(GridView<ItemType> gridView)
 		{
 			var type = typeof(ItemType);
-			var propertiesInfo = type.GetProperties(BindingFlags.Instance | BindingFlags.Public).OrderBy(x => x.MetadataToken);
-			foreach (var propertyInfo in propertiesInfo)
+			var propertiesInfo = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+			if (propertiesInfo.Count() == 0)
+			{
+				propertiesInfo = gridView.DataSource.First()?.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public) ?? Enumerable.Empty<PropertyInfo>().ToArray();
+			}
+
+			foreach (var propertyInfo in propertiesInfo.Where(p => p.Name != IndexerPropertyName).OrderBy(x => x.MetadataToken))
 			{
 				var newColumn = new BoundField<ItemType> {
 					DataField = propertyInfo.Name,
