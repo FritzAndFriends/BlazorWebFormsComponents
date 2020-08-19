02fc80cdbbc376c75c86ed0661fd62cec6e9432f
Matthew Pauloksy <matthew.j.paulosky@boeing.com>
2020-08-19 12:14:20 -0700

    Add Bootstrap CSS styled Menu to the page

--- samples/BeforeWebForms/ControlSamples/Menu/Default.aspx.designer.cs
+++ samples/BeforeWebForms/ControlSamples/Menu/Default.aspx.designer.cs
@@ -11,88 +11,97 @@ namespace BeforeWebForms.ControlSamples.Menu
 {
 
 
-    public partial class Default
-    {
+	public partial class Default
+	{
 
-        /// <summary>
-        /// HorizontalMenuTable control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu HorizontalMenuTable;
+		/// <summary>
+		/// HorizontalMenuTable control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu HorizontalMenuTable;
 
-        /// <summary>
-        /// HorizontalMenuList control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu HorizontalMenuList;
+		/// <summary>
+		/// HorizontalMenuList control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu HorizontalMenuList;
 
-        /// <summary>
-        /// VerticalMenuTable control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu VerticalMenuTable;
+		/// <summary>
+		/// VerticalMenuTable control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu VerticalMenuTable;
 
-        /// <summary>
-        /// VerticalMenuList control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu VerticalMenuList;
+		/// <summary>
+		/// VerticalMenuList control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu VerticalMenuList;
 
-        /// <summary>
-        /// NavigationMenuTableDataSource control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu NavigationMenuTableDataSource;
+		/// <summary>
+		/// NavigationMenuTableDataSource control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu NavigationMenuTableDataSource;
 
-        /// <summary>
-        /// NavigationMenuListDataSource control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu NavigationMenuListDataSource;
+		/// <summary>
+		/// NavigationMenuListDataSource control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu NavigationMenuListDataSource;
 
-        /// <summary>
-        /// VerticalMenuTableDataSource control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu VerticalMenuTableDataSource;
+		/// <summary>
+		/// VerticalMenuTableDataSource control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu VerticalMenuTableDataSource;
 
-        /// <summary>
-        /// VerticalMenuListDataSource control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu VerticalMenuListDataSource;
+		/// <summary>
+		/// VerticalMenuListDataSource control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu VerticalMenuListDataSource;
 
-        /// <summary>
-        /// MenuSource control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.XmlDataSource MenuSource;
-    }
+		/// <summary>
+		/// Menu1 control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Menu Menu1;
+
+		/// <summary>
+		/// MenuSource control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.XmlDataSource MenuSource;
+	}
 }
