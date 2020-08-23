ff977ad5d938b0d66a8434f1300a9f0472ceac1f
hishamco <hishamco_2007@yahoo.com>
2020-08-23 23:06:12 +0300

    Implement IHasStyle

--- src/BlazorWebFormsComponents/AdRotator.razor.cs
+++ src/BlazorWebFormsComponents/AdRotator.razor.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Components;
+using BlazorWebFormsComponents.Enums;
+using BlazorComponentUtilities;
 
 namespace BlazorWebFormsComponents
 {
-	public partial class AdRotator : BaseWebFormsComponent
+	public partial class AdRotator : BaseWebFormsComponent, IHasStyle
 	{
 		[Parameter]
 		public string AdvertisementFile { get; set; }
@@ -17,6 +19,53 @@ namespace BlazorWebFormsComponents
 
 		[Parameter]
 		public string Target { get; set; }
+
+		[Parameter]
+		public WebColor BackColor { get; set; }
+
+		[Parameter]
+		public WebColor BorderColor { get; set; }
+
+		[Parameter]
+		public BorderStyle BorderStyle { get; set; }
+
+		[Parameter]
+		public Unit BorderWidth { get; set; }
+
+		[Parameter]
+		public string CssClass { get; set; }
+
+		[Parameter]
+		public WebColor ForeColor { get; set; }
+
+		[Parameter]
+		public Unit Height { get; set; }
+
+		[Parameter]
+		public Unit Width { get; set; }
+
+		[Parameter]
+		public bool Font_Bold { get; set; }
+
+		[Parameter]
+		public bool Font_Italic { get; set; }
+
+		[Parameter]
+		public string Font_Names { get; set; }
+
+		[Parameter]
+		public bool Font_Overline { get; set; }
+
+		[Parameter]
+		public FontUnit Font_Size { get; set; }
+
+		[Parameter]
+		public bool Font_Strikeout { get; set; }
+
+		[Parameter]
+		public bool Font_Underline { get; set; }
+
+		private string CalculatedStyle => this.ToStyle().Build().NullIfEmpty();
 
 		internal Advertisment GetActiveAdvertisment()
 		{
