7d2f6e0047b1cc5296de17022a8241a61575fe31
hishamco <hishamco_2007@yahoo.com>
2020-08-24 03:04:39 +0300

    Add AdRotator AdCreated event

--- src/BlazorWebFormsComponents/AdRotator.razor.cs
+++ src/BlazorWebFormsComponents/AdRotator.razor.cs
@@ -4,46 +4,67 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorWebFormsComponents
 {
-  public partial class AdRotator : BaseWebFormsComponent
-  {
-	[Parameter]
-	public string AdvertisementFile { get; set; }
-
-	[Parameter]
-	public string Target { get; set; }
-
-	internal Advertisment GetActiveAdvertisment()
+	public partial class AdRotator : BaseWebFormsComponent
 	{
-	  var advertisments = GetAdvertismentsFileContent(AdvertisementFile);
+		[Parameter]
+		public string AdvertisementFile { get; set; }
 
-	  if (advertisments == null || advertisments.Count() == 0)
-	  {
-		return null;
-	  }
+		[Parameter]
+		public string Target { get; set; }
 
-	  var rnd = new Random().Next(advertisments.Count());
+		[Parameter]
+		public EventCallback<AdCreatedEventArgs> OnAdCreated { get; set; }
 
-	  return advertisments.ElementAt(rnd);
+		internal Advertisment GetActiveAdvertisment()
+		{
+			var advertisments = GetAdvertismentsFileContent(AdvertisementFile);
+
+			if (advertisments == null || advertisments.Count() == 0)
+			{
+				return null;
+			}
+
+			var rnd = new Random().Next(advertisments.Count());
+			var advertisment = advertisments.ElementAt(rnd);
+			var adProperties = new Dictionary<string, string>();
+			foreach (var property in typeof(Advertisment).GetProperties())
+			{
+				adProperties.Add(property.Name, property.GetValue(advertisment).ToString());
+			}
+
+			AdCreated(new AdCreatedEventArgs(adProperties) {
+				AlternateText = advertisment.AlternateText,
+				ImageUrl = advertisment.ImageUrl,
+				NavigateUrl = advertisment.NavigateUrl
+			});
+
+			return advertisment;
+		}
+
+		protected void AdCreated(AdCreatedEventArgs e)
+		{
+			OnAdCreated.InvokeAsync(e);
+		}
+
+		private static IEnumerable<Advertisment> GetAdvertismentsFileContent(string fileName)
+		{
+			var xmlDocument = XDocument.Load(new StreamReader(fileName));
+
+			return xmlDocument.Descendants("Ad")
+				.Select(a => new Advertisment
+				{
+					ImageUrl = a.Descendants("ImageUrl").FirstOrDefault()?.Value,
+					Height = a.Descendants("Height").FirstOrDefault()?.Value,
+					Width = a.Descendants("Width").FirstOrDefault()?.Value,
+					NavigateUrl = a.Descendants("NavigateUrl").FirstOrDefault()?.Value,
+					AlternateText = a.Descendants("AlternateText").FirstOrDefault()?.Value,
+					Impressions = a.Descendants("Impressions").FirstOrDefault()?.Value,
+					Keyword = a.Descendants("Keyword").FirstOrDefault()?.Value
+				});
+		}
 	}
-
-	private static IEnumerable<Advertisment> GetAdvertismentsFileContent(string fileName)
-	{
-	  var xmlDocument = XDocument.Load(new StreamReader(fileName));
-
-	  return xmlDocument.Descendants("Ad")
-		  .Select(a => new Advertisment
-		  {
-			ImageUrl = a.Descendants("ImageUrl").FirstOrDefault()?.Value,
-			Height = a.Descendants("Height").FirstOrDefault()?.Value,
-			Width = a.Descendants("Width").FirstOrDefault()?.Value,
-			NavigateUrl = a.Descendants("NavigateUrl").FirstOrDefault()?.Value,
-			AlternateText = a.Descendants("AlternateText").FirstOrDefault()?.Value,
-			Impressions = a.Descendants("Impressions").FirstOrDefault()?.Value,
-			Keyword = a.Descendants("Keyword").FirstOrDefault()?.Value
-		  });
-	}
-  }
 }
