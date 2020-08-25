7a312622f9d3eebb2540edaed23c96dc40762e20
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-08-25 19:12:08 +0300

    Add AlternateTextField, ImageUrlField and NavigateUrlField AdRotator properties (#213)
    * Add AlternateText, ImageUrl and NavigateUrl properties

--- src/BlazorWebFormsComponents/AdRotator.razor.cs
+++ src/BlazorWebFormsComponents/AdRotator.razor.cs
@@ -7,43 +7,71 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorWebFormsComponents
 {
-  public partial class AdRotator : BaseWebFormsComponent
-  {
-	[Parameter]
-	public string AdvertisementFile { get; set; }
-
-	[Parameter]
-	public string Target { get; set; }
-
-	internal Advertisment GetActiveAdvertisment()
+	public partial class AdRotator : BaseWebFormsComponent
 	{
-	  var advertisments = GetAdvertismentsFileContent(AdvertisementFile);
+		private static readonly string DefaultAlternateTextField = "AlternateText";
+		private static readonly string DefaultImageUrlField = "ImageUrl";
+		private static readonly string DefaultNavigateUrlField = "NavigateUrl";
 
-	  if (advertisments == null || advertisments.Count() == 0)
-	  {
-		return null;
-	  }
+		[Parameter]
+		public string AlternateTextField { get; set; } = DefaultAlternateTextField;
 
-	  var rnd = new Random().Next(advertisments.Count());
+		[Parameter]
+		public string AdvertisementFile { get; set; }
 
-	  return advertisments.ElementAt(rnd);
+		[Parameter]
+		public string ImageUrlField { get; set; } = DefaultImageUrlField;
+
+		[Parameter]
+		public string NavigateUrlField { get; set; } = DefaultNavigateUrlField;
+
+		[Parameter]
+		public string Target { get; set; }
+
+		internal Advertisment GetActiveAdvertisment()
+		{
+			if (string.IsNullOrEmpty(AlternateTextField))
+			{
+				throw new ArgumentException("AlternateTextField can't be null or empty.", nameof(AlternateTextField));
+			}
+
+			if (string.IsNullOrEmpty(ImageUrlField))
+			{
+				throw new ArgumentException("AlternateTextField can't be null or empty.", nameof(ImageUrlField));
+			}
+
+			if (string.IsNullOrEmpty(NavigateUrlField))
+			{
+				throw new ArgumentException("AlternateTextField can't be null or empty.", nameof(NavigateUrlField));
+			}
+
+			var advertisments = GetAdvertismentsFileContent(AdvertisementFile);
+
+			if (advertisments == null || advertisments.Count() == 0)
+			{
+				return null;
+			}
+
+			var rnd = new Random().Next(advertisments.Count());
+
+			return advertisments.ElementAt(rnd);
+		}
+
+		private IEnumerable<Advertisment> GetAdvertismentsFileContent(string fileName)
+		{
+			var xmlDocument = XDocument.Load(new StreamReader(fileName));
+
+			return xmlDocument.Descendants("Ad")
+				.Select(a => new Advertisment
+				{
+					ImageUrl = a.Descendants(ImageUrlField).FirstOrDefault()?.Value,
+					Height = a.Descendants("Height").FirstOrDefault()?.Value,
+					Width = a.Descendants("Width").FirstOrDefault()?.Value,
+					NavigateUrl = a.Descendants(NavigateUrlField).FirstOrDefault()?.Value,
+					AlternateText = a.Descendants(AlternateTextField).FirstOrDefault()?.Value,
+					Impressions = a.Descendants("Impressions").FirstOrDefault()?.Value,
+					Keyword = a.Descendants("Keyword").FirstOrDefault()?.Value
+				});
+		}
 	}
-
-	private static IEnumerable<Advertisment> GetAdvertismentsFileContent(string fileName)
-	{
-	  var xmlDocument = XDocument.Load(new StreamReader(fileName));
-
-	  return xmlDocument.Descendants("Ad")
-		  .Select(a => new Advertisment
-		  {
-			ImageUrl = a.Descendants("ImageUrl").FirstOrDefault()?.Value,
-			Height = a.Descendants("Height").FirstOrDefault()?.Value,
-			Width = a.Descendants("Width").FirstOrDefault()?.Value,
-			NavigateUrl = a.Descendants("NavigateUrl").FirstOrDefault()?.Value,
-			AlternateText = a.Descendants("AlternateText").FirstOrDefault()?.Value,
-			Impressions = a.Descendants("Impressions").FirstOrDefault()?.Value,
-			Keyword = a.Descendants("Keyword").FirstOrDefault()?.Value
-		  });
-	}
-  }
 }
