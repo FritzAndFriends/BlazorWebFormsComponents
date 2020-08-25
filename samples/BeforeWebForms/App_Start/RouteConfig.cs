663470bc8821d6e07e43fbac700f8bff2addef55
Eilon Lipton <Eilon@users.noreply.github.com>
2020-08-25 15:10:50 -0700

    Fix formatting of BeforeWebForms folder

--- samples/BeforeWebForms/App_Start/RouteConfig.cs
+++ samples/BeforeWebForms/App_Start/RouteConfig.cs
@@ -6,13 +6,13 @@ using Microsoft.AspNet.FriendlyUrls;
 
 namespace BeforeWebForms
 {
-    public static class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            var settings = new FriendlyUrlSettings();
-            settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
-        }
-    }
+	public static class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			var settings = new FriendlyUrlSettings();
+			settings.AutoRedirectMode = RedirectMode.Permanent;
+			routes.EnableFriendlyUrls(settings);
+		}
+	}
 }
--- samples/BeforeWebForms/ControlSamples/Menu/Default.aspx.cs
+++ samples/BeforeWebForms/ControlSamples/Menu/Default.aspx.cs
@@ -3,7 +3,7 @@ using System.Web.UI.WebControls;
 
 namespace BeforeWebForms.ControlSamples.Menu
 {
-  public partial class Default : System.Web.UI.Page
+	public partial class Default : System.Web.UI.Page
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
