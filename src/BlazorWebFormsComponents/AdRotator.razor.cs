3f7181858091620fd85c78ab013f3aa7384b5217
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-08-25 20:16:33 +0300

    AdRotator AdCreated event (#214)
    * Add AdRotator AdCreated event
    * Override Ad properties

--- src/BlazorWebFormsComponents/AdRotator.razor.cs
+++ src/BlazorWebFormsComponents/AdRotator.razor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace BlazorWebFormsComponents
 {
@@ -12,12 +13,12 @@ namespace BlazorWebFormsComponents
 		private static readonly string DefaultAlternateTextField = "AlternateText";
 		private static readonly string DefaultImageUrlField = "ImageUrl";
 		private static readonly string DefaultNavigateUrlField = "NavigateUrl";
+    
+		[Parameter]
+		public string AdvertisementFile { get; set; }
 
 		[Parameter]
 		public string AlternateTextField { get; set; } = DefaultAlternateTextField;
-
-		[Parameter]
-		public string AdvertisementFile { get; set; }
 
 		[Parameter]
 		public string ImageUrlField { get; set; } = DefaultImageUrlField;
@@ -27,10 +28,13 @@ namespace BlazorWebFormsComponents
 
 		[Parameter]
 		public string Target { get; set; }
-
-		internal Advertisment GetActiveAdvertisment()
-		{
-			if (string.IsNullOrEmpty(AlternateTextField))
+    
+    [Parameter]
+		public EventCallback<AdCreatedEventArgs> OnAdCreated { get; set; }
+    
+    internal Advertisment GetActiveAdvertisment()
+	{
+      if (string.IsNullOrEmpty(AlternateTextField))
 			{
 				throw new ArgumentException("AlternateTextField can't be null or empty.", nameof(AlternateTextField));
 			}
@@ -53,8 +57,33 @@ namespace BlazorWebFormsComponents
 			}
 
 			var rnd = new Random().Next(advertisments.Count());
+			var advertisment = advertisments.ElementAt(rnd);
+			var adProperties = new Dictionary<string, string>();
+			foreach (var property in typeof(Advertisment).GetProperties())
+			{
+				adProperties.Add(property.Name, property.GetValue(advertisment).ToString());
+			}
 
-			return advertisments.ElementAt(rnd);
+			var adArgs = new AdCreatedEventArgs(adProperties)
+			{
+				AlternateText = advertisment.AlternateText,
+				ImageUrl = advertisment.ImageUrl,
+				NavigateUrl = advertisment.NavigateUrl
+			};
+
+			AdCreated(adArgs);
+
+			// Override Ad properties before render
+			advertisment.AlternateText = adArgs.AlternateText;
+			advertisment.ImageUrl = adArgs.ImageUrl;
+			advertisment.NavigateUrl = adArgs.NavigateUrl;
+
+			return advertisment;
+		}
+
+		protected void AdCreated(AdCreatedEventArgs e)
+		{
+			OnAdCreated.InvokeAsync(e);
 		}
 
 		private IEnumerable<Advertisment> GetAdvertismentsFileContent(string fileName)
