d6af57be559da36a09afbafca50205134c6c87a4
hishamco <hishamco_2007@yahoo.com>
2020-08-28 00:26:27 +0300

    ImageButton should implements IHasStyle

--- src/BlazorWebFormsComponents/ImageButton.razor.cs
+++ src/BlazorWebFormsComponents/ImageButton.razor.cs
@@ -1,9 +1,10 @@
+using BlazorComponentUtilities;
 using BlazorWebFormsComponents.Enums;
 using BlazorWebFormsComponents.Interfaces;
 using Microsoft.AspNetCore.Components;
 namespace BlazorWebFormsComponents
 {
-	public partial class ImageButton : ButtonBaseComponent, IImageComponent
+	public partial class ImageButton : ButtonBaseComponent, IImageComponent, IHasStyle
 	{
 		[Parameter]
 		public string AlternateText { get; set; }
@@ -16,5 +17,52 @@ namespace BlazorWebFormsComponents
 
 		[Parameter]
 		public string ImageUrl { get; set; }
+
+		[Parameter]
+		public WebColor BackColor { get; set; }
+
+		[Parameter]
+		public WebColor BorderColor { get; set; }
+
+		[Parameter]
+		public BorderStyle BorderStyle { get; set; }
+
+		[Parameter]
+		public Unit BorderWidth { get; set; }
+
+		[Parameter]
+		public string CssClass { get; set; }
+
+		[Parameter]
+		public WebColor ForeColor { get; set; }
+
+		[Parameter]
+		public Unit Height { get; set; }
+
+		[Parameter]
+		public Unit Width { get; set; }
+
+		[Parameter]
+		public bool Font_Bold { get; set; }
+
+		[Parameter]
+		public bool Font_Italic { get; set; }
+
+		[Parameter]
+		public string Font_Names { get; set; }
+
+		[Parameter]
+		public bool Font_Overline { get; set; }
+
+		[Parameter]
+		public FontUnit Font_Size { get; set; }
+
+		[Parameter]
+		public bool Font_Strikeout { get; set; }
+
+		[Parameter]
+		public bool Font_Underline { get; set; }
+
+		private string CalculatedStyle => this.ToStyle().Build().NullIfEmpty();
 	}
 }
